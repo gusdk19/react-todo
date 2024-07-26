3a65ac868269eadca740391379512a685618511e
gusdk19 <hyuna030619@naver.com>
2024-07-26 12:26:39 +0900

    #3. Todo 조회 기능 구현

---
+++ src/components/todos/TodoBody.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import TodoItem from './TodoItem'
+
+// const todos = [1, 2, 3];
+const TodoBody = ({todos}) => {
+  return (
+    <ul className='px-0 my-8'>
+        {/* li태그를 todos 배열의 요소만큼 렌더링 */}
+        {todos.map(todo => <TodoItem key={todo.id} todo={todo}/>)}
+    </ul>
+  )
+}
+
+export default TodoBody
